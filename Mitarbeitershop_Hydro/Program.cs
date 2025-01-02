7d78ac6aed33c108bbbca062821d94f2a8fbf3a6
Dima <dmytro.shokhov@viega.de>
2025-01-02 12:18:16 +0100

    Add Hydro

--- Mitarbeitershop_Hydro/Program.cs
+++ Mitarbeitershop_Hydro/Program.cs
@@ -1,9 +1,17 @@
+using Hydro.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHydro();
+
+
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +20,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
@@ -25,5 +36,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+app.UseHydro(builder.Environment);
 
 app.Run();
